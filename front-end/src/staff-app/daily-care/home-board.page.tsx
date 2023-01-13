e7f1645d4d78b446d363da24de97e531d9cdb08f
sudheer kumar <sudheerkvy1996@gmail.com>
2023-01-13 15:52:45 +0530

    added search functionality to the daily screen to search students in the list

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import Button from "@material-ui/core/ButtonBase"
-import { RadioGroup, Radio, FormControlLabel, TextField } from "@material-ui/core"
+import InputBase from "@material-ui/core/InputBase"
+
+import { RadioGroup, Radio, FormControlLabel } from "@material-ui/core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
 import { Colors } from "shared/styles/colors"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
-import { Person } from "shared/models/person"
+import { Person, PersonHelper } from "shared/models/person"
 import { useApi } from "shared/hooks/use-api"
 import { StudentListTile } from "staff-app/components/student-list-tile/student-list-tile.component"
 import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active-roll-overlay/active-roll-overlay.component"
@@ -15,6 +17,7 @@ import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward"
 
 export const HomeBoardPage: React.FC = () => {
   const [isRollMode, setIsRollMode] = useState(false)
+  const [searchKey, setKey] = useState<string>("")
   const [name, setName] = useState<student_name>("first_name")
   const [ascending, setAscending] = useState<boolean>(true)
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
@@ -50,11 +53,20 @@ export const HomeBoardPage: React.FC = () => {
       setIsRollMode(false)
     }
   }
+  const onSearchName = (key: string) => {
+    setKey(key)
+    let all_students: Person[] = [...(data?.students || [])]
+    if (key.trim().length !== 0) {
+      setStudents(all_students.filter((student) => PersonHelper.getFullName(student).toLowerCase().includes(key.toLowerCase())))
+    } else {
+      setStudents(all_students)
+    }
+  }
   // console.log("all data", data)
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} ascending={ascending} name={name} />
+        <Toolbar onItemClick={onToolbarAction} ascending={ascending} name={name} searchKey={searchKey} onSearchName={onSearchName} />
 
         {loadState === "loading" && (
           <CenteredContainer>
@@ -87,10 +99,12 @@ interface ToolbarProps {
   ascending: boolean
   name: student_name
   onItemClick: (action: ToolbarAction, value: student_name) => void
+  searchKey: string
+  onSearchName: (key: string) => void
 }
 
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick, ascending, name } = props
+  const { onItemClick, ascending, name, searchKey, onSearchName } = props
   return (
     <S.ToolbarContainer>
       <div
@@ -105,9 +119,15 @@ const Toolbar: React.FC<ToolbarProps> = (props) => {
         <FormControlLabel value="first_name" control={<Radio size="small" />} label="First Name" />
         <FormControlLabel value="last_name" control={<Radio size="small" />} label="Last Name" />
       </RadioGroup>
-      <div>
-        <TextField variant="outlined" style={{ width: "100px", background: "white" }} size="small" placeholder="Search" />
-      </div>
+
+      <S.TextField
+        value={searchKey}
+        onChange={(e) => {
+          onSearchName(e.target.value)
+        }}
+        placeholder="Search"
+      />
+
       <S.Button onClick={() => onItemClick("roll", name)}>Start Roll</S.Button>
     </S.ToolbarContainer>
   )
@@ -135,6 +155,17 @@ const S = {
       padding: ${Spacing.u2};
       font-weight: ${FontWeight.strong};
       border-radius: ${BorderRadius.default};
+    }
+  `,
+  TextField: styled(InputBase)`
+    && {
+      padding: ${Spacing.u2};
+      font-weight: ${FontWeight.normal};
+      border-radius: ${BorderRadius.default};
+      background-color: white;
+      height:30px;
+      width:100px;
+      margin:0,
     }
   `,
 }
